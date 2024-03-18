9243d419dd37d701d4d0c65ae87c32db975b06e6
Ahthesham Ali <ahtheshamali786@gmail.com>
2024-03-18 19:58:42 -0400

    made more changes

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -56,7 +56,9 @@ export default function Page() {
           </div>
 
           <div ref={achievementsSectionRef}>
+            <div className="max-w-7xl mx-auto p-5 mt-20">
             <Achievements />
+            </div>
           </div>
 
           <Footer />
