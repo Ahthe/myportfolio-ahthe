061636046abcf5462b79cc9b33423eed2b67731b
Ahthesham Ali <ahtheshamali786@gmail.com>
2024-03-18 20:02:50 -0400

    made more changes

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -52,13 +52,13 @@ export default function Page() {
           </div>
 
           <div ref={experienceSectionRef}>
+          <div className="max-w-7xl mx-auto p-5 mt-16">
             <Experience />
+            </div>
           </div>
 
           <div ref={achievementsSectionRef}>
-            <div className="max-w-7xl mx-auto p-5 mt-20">
             <Achievements />
-            </div>
           </div>
 
           <Footer />
